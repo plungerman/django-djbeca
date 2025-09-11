b313f967754b7228cd6bdccb25667471a82daf24
plungerman <plungerman@gmail.com>
2025-09-11 08:52:25 -0500

    removed debugging logging

--- djbeca/core/views.py
+++ djbeca/core/views.py
@@ -508,7 +508,7 @@ def proposal_form(request, pid=None):
                 else:
                     # staff do not have a dean so we send the email
                     # to OSP folks
-                    to_list.append(PROPOSAL_EMAIL_LIST)
+                    to_list = to_list + PROPOSAL_EMAIL_LIST
                     if DEBUG:
                         data.to_list = to_list
                         to_list = TEST_EMAILS
@@ -526,8 +526,6 @@ def proposal_form(request, pid=None):
                     Your Approval Needed for "{0}" by {1}, {2}'.format(
                     data.title, data.user.last_name, data.user.first_name,
                 )
-
-                logger.debug('to_list = {0}'.format(to_list))
                 if not data.save_submit:
                     frum = PROPOSAL_EMAIL_LIST[0]
                     send_mail(
